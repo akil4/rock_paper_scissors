16368585deaad66bebd0638d13dd8360f6295554
akil4 <akils4388@gmail.com>
2024-10-06 14:28:25 +0530

    add scoreboard function

--- main.js
+++ main.js
@@ -1,29 +1,29 @@
-console.log("Hello, World!");
 let humanScore = 0;
-computerScore = 0;
+let computerScore = 0;
 let computerChoice = ""; 
 let humanChoice = "";
 
+getHumanChoice();
 // Function to get User's Input
 function getHumanChoice() {
     let array = ["rock", "paper", "scissors"];
     let choice = prompt("Enter your choice (rock or paper or scissors): ");
     choice = choice.toLowerCase();
-    if (choice == array[0]) {
+    if (choice == array[0] || choice == "r") {
         humanChoice = "Rock";
     }
-    else if (choice == array[1]) {
+    else if (choice == array[1] || choice == "p") {
         humanChoice = "Paper";
     }
-    else if (choice == array[2]) {
+    else if (choice == array[2] || choice == "s") {
         humanChoice = "Scissors";
     }
     else {
         getHumanChoice();
     }
+    console.log(humanChoice);
+    getComputerChoice();
 }
-getHumanChoice();
-console.log(humanChoice);
 
 // Function to get choice from the computer
 function getComputerChoice() {
@@ -37,6 +37,47 @@ function getComputerChoice() {
     else {
         computerChoice = "Scissors";
     }
+    console.log(computerChoice);
+    playRound(humanChoice, computerChoice);
 }
-getComputerChoice();
-console.log(computerChoice);
+
+// Function to return scoreboard
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice == "Rock" && computerChoice == "Rock") {
+        console.log("Draw! Play Again.")
+    }
+    else if (humanChoice == "Rock" && computerChoice == "Paper") {
+        console.log("You lose! Paper beats Rock.")
+        computerScore++;
+    }
+    else if (humanChoice == "Rock" && computerChoice == "Scissors") {
+        console.log("You win! Rock beats Scissors.")
+        humanScore++;
+    }
+    else if (humanChoice == "Paper" && computerChoice == "Rock") {
+        console.log("You win! Paper beats Rock.")
+        humanScore++;
+    }
+    else if (humanChoice == "Paper" && computerChoice == "Paper") {
+        console.log("Draw! Play Again.")
+    }
+    else if (humanChoice == "Paper" && computerChoice == "Scissors") {
+        console.log("You lose! Scissors beat Paper.")
+        computerScore++;
+    }
+    else if (humanChoice == "Scissors" && computerChoice == "Rock") {
+        console.log("You lose! Rock beats Scissors.")
+        computerScore++;
+    }
+    else if (humanChoice == "Scissors" && computerChoice == "Paper") {
+        console.log("You win! Scissors beat Paper.")
+        humanScore++;
+    }
+    else if (humanChoice == "Scissors" && computerChoice == "Scissors") {
+        console.log("Draw! Play Again.")
+    }
+    console.log("Score: ");
+    console.log(`You: ${humanScore}`);
+    console.log(`Computer: ${computerScore}`);
+    getHumanChoice();
+}
